a51b1593ed4964462f1e66ddd5399fa2544bb4d5
Less Wright <lessw@etrillium.com>
2023-07-04 18:02:53 -0700

    first 'working' draft

--- src/cwcAdam.py
+++ src/cwcAdam.py
@@ -2,6 +2,8 @@
 import torch
 from torch import Tensor
 from torch.optim.optimizer import Optimizer
+import numpy as np
+from typing import List
 
 class CoordinateWiseClipping_AdamW(Optimizer):
     """
@@ -55,7 +57,6 @@ class CoordinateWiseClipping_AdamW(Optimizer):
                         eps=eps,
                         weight_decay=weight_decay,
                         top_k_percent=percent_to_clip,
-                        no_prox=no_prox,
                         decoupled_wd=use_decoupled_weight_decay)
         super().__init__(params, defaults)
 
@@ -63,5 +64,152 @@ class CoordinateWiseClipping_AdamW(Optimizer):
         super(CoordinateWiseClipping_AdamW, self).__setstate__(state)
         for group in self.param_groups:
             group.setdefault('use_decoupled_weight_decay', True)
+
+    
+    @torch.no_grad()
+    def restart_optimizer(self):
+        for group in self.param_groups:
+            group['step'] = 0
+            for p in group['params']:
+                if p.requires_grad:
+                    state = self.state[p]
+                    # State initialization
+
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p)
+    
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step."""
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        if self.defaults['top_k_percent'] > 0:
+            device = self.param_groups[0]['params'][0].device
+            global_coord_threshold = torch.zeros(1, device=device)
+
+            top_k_percentage = torch.tensor(self.defaults['top_k_percent'],
+                                         device=device)
+            raw_grad_sizes = []
+            
+            # bool where True = +
+            raw_grad_sign = []
+            for group in self.param_groups:
+
+                for p in group['params']:
+                    if p.grad:
+                        grad = p.grad
+                        raw_grad_sign.append(grad > 0)
+                        raw_grad_sizes.append(torch.abs(grad))
+                   
+            global_coord_max = np.percentile(raw_grad_sizes, global_coord_threshold, method = "nearest")
+            
+        for group in self.param_groups:
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            
+            beta1, beta2 = group['betas']
+            
+            if 'step' in group:
+                group['step'] += 1
+            else:
+                group['step'] = 1
+
+            bias_correction1 = 1.0 - beta1**group['step']
+            bias_correction2 = 1.0 - beta2**group['step']
+            
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                params_with_grad.append(p)
+                grads.append(p.grad)
+
+                state = self.state[p]
+                if len(state) == 0:
+                    state['exp_avg'] = torch.zeros_like(p)
+                    state['exp_avg_sq'] = torch.zeros_like(p)
+                    state['exp_avg_diff'] = torch.zeros_like(p)
+
+                exp_avgs.append(state['exp_avg'])
+                exp_avg_sqs.append(state['exp_avg_sq'])
+                
+
+            kwargs = dict(
+                params=params_with_grad,
+                grads=grads,
+                exp_avgs=exp_avgs,
+                exp_avg_sqs=exp_avg_sqs,
+                
+                beta1=beta1,
+                beta2=beta2,
+               
+                bias_correction1=bias_correction1,
+                bias_correction2=bias_correction2,
+                
+                lr=group['lr'],
+                weight_decay=group['weight_decay'],
+                eps=group['eps'],
+                decouple_wd=group['decouple_wd'],
+                global_coord_max=global_coord_max,
+                raw_grad_sizes = raw_grad_sizes, 
+                raw_grad_sign = raw_grad_signs,
+                
+            )
+
+            
+        _single_tensor_cwcAdamW(**kwargs)
+
+        return loss
+
+
+def _single_tensor_cwcAdamW(
+    params: List[Tensor],
+    grads: List[Tensor],
+    exp_avgs: List[Tensor],
+    exp_avg_sqs: List[Tensor],
+    *,
+    beta1: float,
+    beta2: float,
+    bias_correction1: float,
+    bias_correction2: float,
+    lr: float,
+    weight_decay: float,
+    eps: float,
+    decouple_wd=group['decouple_wd'],
+    global_coord_max,
+    raw_grad_sizes,
+    raw_grad_sign,
+):
+    for i, param in enumerate(params):
+        grad = grads[i]
+        exp_avg = exp_avgs[i]
+        exp_avg_sq = exp_avg_sqs[i]
+        k_grad = grad
         
+        torch.clamp(k_grad, min = -global_coord_max, max = global_coord_max)
+        
+
+        exp_avg.mul_(beta1).add_(k_grad, alpha=1 - beta1)  # momentum_term
+        
+        exp_avg_sq.mul_(beta2).add_(grad, alpha=1-beta2)  # variance_term
+
+        denom = (exp_avg_sq.sqrt().add_(eps))
+        step_size_diff = lr * beta2 / bias_correction2
+        step_size = lr / bias_correction1
+
+        if not decouple_wd:
+            param.mul_(1 - lr * weight_decay)
+            param.addcdiv_(exp_avg, denom, value=-step_size)
+            
+        else:
+            param.addcdiv_(exp_avg, denom, value=-step_size)
+            
+            param.div_(1 + lr * weight_decay)
 
